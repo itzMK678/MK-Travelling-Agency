fe6f825c8bc43c0a619d4b1c480a79950fc0e9d7
ItzMK <raomamoon592@gmail.com>
2026-06-23 23:27:55 +0500

    button responsiveness

--- src/app/components/BackButton.jsx
+++ src/app/components/BackButton.jsx
@@ -10,7 +10,15 @@ const BackButton = () => {
   return (
     <button
       onClick={() => router.back()}
-      className="absolute z-10 left-25 top-7 bg-red-500 p-3 rounded-full cursor-pointer"
+      className="
+  absolute z-10
+  left-[3%]
+  top-[3%]
+  bg-red-500
+  p-2 md:p-3
+  rounded-full
+  cursor-pointer
+"
     >
       <ArrowLeft />
     </button>
